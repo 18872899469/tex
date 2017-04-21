becaeacf25833527e17219ce9c59c55419097e3a
18872899469 <470190620@qq.com>
2017-04-21 15:40:18 +0800

    45

---
+++ text.js
@@ -0,0 +1,28 @@
+
+
+function test(x){
+    document.title ="正规网站";
+    document.getElementById("xx").style.display="block";
+   document.getElementById("cad").style.backgroundColor= "steelblue"; 
+    document.getElementById("zz").style.display="none";
+    document.getElementById("vv").style.backgroundColor= "white";
+ 
+
+   
+}
+
+   
+
+function css(x){
+   
+    document.getElementById("zz").style.display="block";
+    document.getElementById("vv").style.backgroundColor= "steelblue";
+     document.getElementById("xx").style.display="none";
+   document.getElementById("cad").style.backgroundColor= "white";
+     
+  
+    }
+    
+    
+   
+    
